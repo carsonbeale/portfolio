52222da8e42bc4f6586fa7d249447e20daf4122e
Mission Bit <missionbit@37a-MissionBit.local>
2019-11-20 15:58:52 -0800

    tst

--- final project /main.js
+++ final project /main.js
@@ -1,9 +1,30 @@
 
 window.onload = function(){
+    const colorThief = new ColorThief();
     const image    = document.querySelector('.preview');
+    const palette = document.querySelector('.palette');
     const input = document.querySelector('[type="file"]');
 
     input.onchange = e =>{
         image.src = URL.createObjectURL(input.files[0])
     }
+
+    const swatches = 12;
+
+
+
+image.onload = () => {
+    URL.revokeObjectURL(image.src);
+    const colors =  colorThief.getPalette(image, swatches);    
+    while (palette.firstChild) palette.removeChild(palette.firstChild);
+    colors.reduce( (palette,rgb) => {
+        const color = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;        
+        const swatch = document.createElement('div');
+        swatch.style.setProperty('--color', color);
+        swatch.setAttribute('color', color);
+        palette.appendChild(swatch);
+        return palette;
+    }, palette)
+}
+
 }
